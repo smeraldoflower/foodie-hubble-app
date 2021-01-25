dd446bfad33529dbd653ce9282487406696a7010
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2021-01-25 15:17:21 -0600

    updated file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 import Login from './components/Login'
 
 function App() {
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <h2>Foodie Hubble App</h2>
-      <img src={logo} className="App-logo" alt="logo" />
       <Login />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,15 +4,6 @@ import { api } from '../utilities/api'
 
 function Login() {
 
-    axios.get('https://foodhub-api.herokuapp.com/restaurant/details/5cc1f333df245c427cc2664b')
-        .then(function (response) {
-            console.log('GET REQUEST HERE:')
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
     axios.post(api.login,
         {
             "username": "Ahmed Sadman",
