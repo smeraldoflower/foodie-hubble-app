3637a09a1a93edebdec73ce9d54b5339059eea04
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2021-01-24 20:31:31 -0600

    added axios library, api connectivity

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  axios.get('https://foodhub-api.herokuapp.com/restaurant/details/5cc1f333df245c427cc2664b')
+    .then(function (response) {
+      console.log('GET REQUEST HERE:')
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  axios.post('https://foodhub-api.herokuapp.com/auth/login',
+    {
+      "username": "Ahmed Sadman",
+      "password": "123456"
+    }
+  )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Foodie Hubble App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label> Username: <input type='text' /> </label><br />
+          <label> Password: <input type='text' /> </label><br />
+          <input type='submit' value='LOGIN' />
+        </form>
       </header>
     </div>
   );
